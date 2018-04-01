f4bc2161eb4c4beaad0f025d5f811663b3606948
pintoo2262 <mcabca5094@gmail.com>
2018-04-01 16:22:58 +0530

    Add files via upload

---
+++ helper/listener/BrandAdapterCallback.java
@@ -0,0 +1,14 @@
+package com.app.noan.listener;
+
+import com.app.noan.model.BrandModel;
+
+import java.util.List;
+
+/**
+ * Created by Suleiman on 16/11/16.
+ */
+
+public interface BrandAdapterCallback {
+
+    void retryBrandId(List<BrandModel> brandList);
+}
---
+++ helper/listener/PaginationAdapterCallback.java
@@ -0,0 +1,10 @@
+package com.app.noan.listener;
+
+/**
+ * Created by Suleiman on 16/11/16.
+ */
+
+public interface PaginationAdapterCallback {
+
+    void retryPageLoad();
+}
